60da43a2230099ca77abfc4d3540d3ffb4dfc4b7
Sushant R. Dangal <srdarkseer@gmail.com>
2024-01-24 00:01:35 +0545

    updated footer

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -10,15 +10,15 @@ const Footer: React.FC = () => {
     <footer className="bg-black text-white py-10">
       <div className="container flex flex-col gap-6">
         <div className="grid grid-cols-1 sm:grid-cols-12 gap-6 md:gap-16">
-          <div className=" col-span-12 sm:col-span-4 xl:col-span-3 flex flex-col gap-9 items-center sm:items-start">
+          <div className="col-span-12 sm:col-span-4 xl:col-span-3 flex flex-col gap-9 items-center sm:items-start">
             <h2 className="font-bold text-xl">WESTON</h2>
             <p className="text-center sm:text-start">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
             </p>
           </div>
 
-          <div className="col-span-7 xl:col-span-5 flex flex-col gap-9">
-            <p>
+          <div className="col-span-12 sm:col-span-7 xl:col-span-5 flex flex-col gap-9">
+            <p className="text-center sm:text-start">
               For exclusive news and market updates sign up for our newsletter.
             </p>
             <div className="flex gap-2">
@@ -36,7 +36,7 @@ const Footer: React.FC = () => {
             </div>
           </div>
 
-          <div className="col-span-8 lg:col-span-7 xl:col-span-4 flex justify-between">
+          <div className="col-span-12 sm:col-span-8 lg:col-span-7 xl:col-span-4 flex justify-between">
             <div className="flex flex-col gap-9">
               <h2 className="font-bold text-xl">Contact Us</h2>
               <div className="flex flex-col gap-2.5">
