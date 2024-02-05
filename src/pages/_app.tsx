0a6de460a2a20c794a604683072ccbbf494e6d19
Sushant R. Dangal <srdarkseer@gmail.com>
2024-02-05 22:35:39 +0545

    demo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Footer from "@/layouts/components/Footer";
+import TopBar from "@/layouts/components/Topbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <TopBar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
